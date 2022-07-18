7d50c534b986626f67b4bdc58ff663f66c3a814e
mohamed shukry <mohamed.shukry412@gmail.com>
2022-07-19 00:27:39 +0200

    Add project files.

---
+++ DatingApp/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using DatingApp.Data;
+using DatingApp.Entites;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext db;
+        public UsersController(DataContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await db.Users.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUserbyId(int id)
+        {
+            return await db.Users.FindAsync(id);
+        }
+    }
+}
---
+++ DatingApp/Entites/AppUser.cs
@@ -0,0 +1,8 @@
+﻿namespace DatingApp.Entites
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+    }
+}
